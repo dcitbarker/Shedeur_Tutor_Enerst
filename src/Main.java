adc0e01f6518842bdfc750065bb0ab0ca5f869b1
dcitbarker <102969822+dcitbarker@users.noreply.github.com>
2023-03-01 15:06:45 +0000

    update

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,26 @@
-public class Main extends Person
-/**
- implement is for multiple blue-prints
- there are two other classes; abstract and interface
-
- */
-{
-    public static void main(String[]args)
-    {
-        String home = "TF";
-        Person person1=new Person();
-        Person person2=new Person();
-
+public class Main extends Persons {
+    public static void main(String[] args){
+        Persons person1= new Persons();
+        Persons person2= new Persons();
         person1.setAge(20);
-        person1.setName("Dela");
-        person1.setId("AD21034C8");
-        person1.setProgramme("Bsc. Computer Science");
+        person1.setName("Ernie");
+        person1.setID("10950924adb");
+        person1.setProgramme("CS with Statistics");
 
         person2.setAge(19);
-        person2.setName("Aba");
-        person2.setId("AD31065D9");
-        person2.setProgramme("Bsc. Food Science");
+        person2.setName("Kwame");
+        person2.setID("2354adf567");
+        person2.setProgramme("Earth Science");
 
-        String Eii = person1.getName();
-
-        System.out.println("Your name is "+Eii);
-        System.out.println("Your age is "+person1.getAge());
-        System.out.println("You offer "+person1.getProgramme());
-        System.out.println("Your Id is "+person1.getId());
+        String name = person1.getName();
 
 
+
+
+        System.out.println(name);
+        System.out.println(person1.getAge());
+        System.out.println(person1.getProgramme());
+        System.out.println(person1.getID());
 
     }
 }
-
---
+++ src/Persons.java
@@ -0,0 +1,39 @@
+public class Persons {
+    private String name;
+   private String ID;
+   private int age;
+   private  String programme;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
